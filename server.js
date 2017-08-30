5bf8d5a591506bb3f03e6f92605e344f69951c0e
arshan <arshankhanifar@gmail.com>
2017-08-30 17:47:07 -0400

    finished the first part

--- server.js
+++ server.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const config = {
+	mongodb: 'mongodb://arshankhanifar:Physics92@ds115214.mlab.com:15214/star-wars-quotes'
+}
+var db;
+
+MongoClient.connect(config.mongodb, (err, database) => {
+	if (err) return console.log('got err\n', err);
+	db = database;
+	app.listen(3000, () => {
+		console.log('listening on 3000');
+	});
+});
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.set('view engine', 'ejs');
+
+app.get('/arshanpage', (req, res) => {
+	res.render('whatsup', {title: 'arshan page title shii', message: "Hey what's up?"});
+});
+
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + '/index.html');
+	var cursor = db.collection('quotes').find().toArray((err, result) => {
+		if (err) return console.log(err)
+		res.render('index', {quotes: result});
+	});
 })
 
 app.post('/quotes', (req,res) => {
-	console.log(req.body);
-
+	db.collection('quotes').save(req.body, (err, result) => {
+		if (err) return console.log(err);
+		console.log('saved to database')
+		res.redirect('/')
+	});
 });
-
-app.listen(3000, function() {
-	console.log('listening on 3000');
-})
 
 
